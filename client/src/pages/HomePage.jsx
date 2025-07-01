d110168bab78a48c8d9effecf37f9465b18c2ea6
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-07-01 14:36:29 +0500

    fixing, overflow scroll styling, part-2

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -96,7 +96,7 @@ const HomePage = () => {
   return (
     <div className="">
       {/* categories based foods */}
-      <div className="flex space-x-4 mb-6 overflow-x-scroll no-scrollbar">
+      <div className="flex space-x-4 mb-6 overflow-x-auto no-scrollbar">
         {categoryOptions.map((item, index) => (
           <div
             onClick={() => setActiveCategory(item.name)}
